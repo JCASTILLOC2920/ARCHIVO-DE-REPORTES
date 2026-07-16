bb3a0048342da42634a2072215a5c578e85ba034
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-07-15 19:47:34 -0500

    s

--- fix3.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('imprimir.html', 'utf8');
-
-html = html.replace(
-    /const urlParams = new URLSearchParams\(window\.location\.search\);\s*const urlParams = new URLSearchParams\(window\.location\.search\);/,
-    "const urlParams = new URLSearchParams(window.location.search);"
-);
-
-fs.writeFileSync('imprimir.html', html);
