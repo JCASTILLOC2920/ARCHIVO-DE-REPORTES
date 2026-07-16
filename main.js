7bee0ea4ce71fc3490f42f3711c56ef9750fc1ea
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-07-16 05:58:27 -0500

    F

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // main.js
 // PROTOCOLO ACTOR-CRITICO: Orquestador Principal (Punto de Entrada Modular)
 
-import { initLocalDatabases, patientDatabase, loadDoctorsData, doctorsDatabase, categoriesDatabase, templatesDatabase, triggerAutomaticBackup } from './db_service.js?v=3.2';
-import { initTableUI, renderTable, applyFilters, setCurrentService } from './ui_tables.js?v=3.2';
-import { initModalListeners, openModal, closeModal } from './ui_editor.js?v=3.2';
-import { openPrintWindow } from './pdf_engine.js?v=3.2';
-import { initDictaphone, startDictation } from './dictaphone_core.js?v=3.2';
-import { initReportEditorLogic, populateEditorModal } from './ui_report_editor.js?v=3.2';
-import { initAdminUI, populateModalDoctorsSelect } from './ui_admin.js?v=3.2';
+import { initLocalDatabases, patientDatabase, loadDoctorsData, doctorsDatabase, categoriesDatabase, templatesDatabase, triggerAutomaticBackup } from './db_service.js?v=3.3';
+import { initTableUI, renderTable, applyFilters, setCurrentService } from './ui_tables.js?v=3.3';
+import { initModalListeners, openModal, closeModal } from './ui_editor.js?v=3.3';
+import { openPrintWindow } from './pdf_engine.js?v=3.3';
+import { initDictaphone, startDictation } from './dictaphone_core.js?v=3.3';
+import { initReportEditorLogic, populateEditorModal } from './ui_report_editor.js?v=3.3';
+import { initAdminUI, populateModalDoctorsSelect } from './ui_admin.js?v=3.3';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("[Core] Inicializando Sistema Modular V2...");
