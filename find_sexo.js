bb9652e8b9bb646494f6819a3fc2afa7f4a4593e
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-07-15 21:33:47 -0500

    d

--- find_sexo.js
+++
@@ -1,5 +0,0 @@
-const js = require('fs').readFileSync('reportes.js', 'utf8');
-const lines = js.split('\n');
-lines.forEach((l, i) => {
-    if (l.includes('re_sexo')) console.log(`Line ${i}: ${l.trim()}`);
-});
