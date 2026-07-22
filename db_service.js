6af5f6adf8bf2b7f694054ae129dbb1fcba0d72f
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-07-21 19:04:59 -0500

    Update db_service.js

--- db_service.js
+++ db_service.js
@@ -148,7 +148,8 @@ export let doctorsDatabase = [];
 export const usersDatabase = [
     { id: 1, perfil: 'Administrador', dni: '41457466', nombres: 'JOSEHP CRISTOPHER CASTILLO CUENCA', usuario: '41457466', clave: 'Betasporina1g' },
     { id: 2, perfil: 'Usuario', dni: '', nombres: 'CLINICA LA MUJER', usuario: 'Mujersegura', clave: '212523' },
-    { id: 3, perfil: 'Usuario', dni: '', nombres: 'CLÍNICA CARRIÓN', usuario: 'clinicacarrion', clave: '212523' }
+    { id: 3, perfil: 'Usuario', dni: '', nombres: 'CLÍNICA CARRIÓN', usuario: 'clinicacarrion', clave: '212523' },
+    { id: 4, perfil: 'Usuario', dni: '', nombres: 'CLÍNICA SAN CLEMENTE', usuario: 'sanclemente', clave: '112603' }
 ];
 
 export const defaultCategories = [
