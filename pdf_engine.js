49a9c6b5c776720fc61743b651cec6ab8329e0a1
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-08-18 18:01:18 -0500

    S

--- pdf_engine.js
+++ pdf_engine.js
@@ -1,6 +1,6 @@
 // pdf_engine.js
 // PROTOCOLO ACTOR-CRITICO: Módulo Aislado para Generación y Enrutamiento de PDF
-import { patientDatabase } from './db_service.js?v=3.80';
+import { patientDatabase } from './db_service.js?v=3.81';
 
 export function openPrintWindow(codAtencion) {
     if (!codAtencion) {
