278251b56d5cd0b63591d64b40534152bd3865de
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-08-18 19:58:28 -0500

    S

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // main.js
 // PROTOCOLO ACTOR-CRITICO: Orquestador Principal (Punto de Entrada Modular)
 
-import { initLocalDatabases, patientDatabase, loadDoctorsData, doctorsDatabase, categoriesDatabase, templatesDatabase, triggerAutomaticBackup, syncPatientsFromSupabase, subscribePatientsRealtime, savePatient, deletePatient, updateSyncStatusUI, fetchFullPatientDetails, processSyncQueue } from './db_service.js?v=3.83';
-import { initTableUI, renderTable, applyFilters, setCurrentService } from './ui_tables.js?v=3.83';
-import { initModalListeners, openModal, closeModal } from './ui_editor.js?v=3.83';
-import { openPrintWindow } from './pdf_engine.js?v=3.83';
-import { initDictaphone, startDictation } from './dictaphone_core.js?v=3.83';
-import { initReportEditorLogic, populateEditorModal } from './ui_report_editor.js?v=3.83';
-import { initAdminUI, populateModalDoctorsSelect } from './ui_admin.js?v=3.83';
+import { initLocalDatabases, patientDatabase, loadDoctorsData, doctorsDatabase, categoriesDatabase, templatesDatabase, triggerAutomaticBackup, syncPatientsFromSupabase, subscribePatientsRealtime, savePatient, deletePatient, updateSyncStatusUI, fetchFullPatientDetails, processSyncQueue } from './db_service.js?v=3.84';
+import { initTableUI, renderTable, applyFilters, setCurrentService } from './ui_tables.js?v=3.84';
+import { initModalListeners, openModal, closeModal } from './ui_editor.js?v=3.84';
+import { openPrintWindow } from './pdf_engine.js?v=3.84';
+import { initDictaphone, startDictation } from './dictaphone_core.js?v=3.84';
+import { initReportEditorLogic, populateEditorModal } from './ui_report_editor.js?v=3.84';
+import { initAdminUI, populateModalDoctorsSelect } from './ui_admin.js?v=3.84';
 
 document.addEventListener('DOMContentLoaded', () => {
     // Aplicar tema guardado al cargar
--- users_db.js
+++ users_db.js
@@ -5,5 +5,6 @@ export const usersDatabase = [
     { id: 4, perfil: 'Usuario', dni: '', nombres: 'CLÍNICA SAN CLEMENTE', usuario: 'sanclemente', clave: '112603' },
     { id: 5, perfil: 'Usuario', dni: '', nombres: 'SR JUNCO', usuario: 'JUNCO2026', clave: 'JUNCO2026' },
     { id: 6, perfil: 'Administrador', dni: '', nombres: 'NICOLAS CASTILLO DIAZ', usuario: 'viringo', clave: '112603' },
-    { id: 7, perfil: 'Usuario', dni: '', nombres: 'DR. DIEGO ALONSO CHUNGUI BRAVO', usuario: 'drdiegochungui', clave: 'diego2026' }
+    { id: 7, perfil: 'Usuario', dni: '', nombres: 'DR. DIEGO ALONSO CHUNGUI BRAVO', usuario: 'drdiegochungui', clave: 'diego2026' },
+    { id: 8, perfil: 'Usuario', dni: '', nombres: 'CLÍNICA ALFA PREVENIR', usuario: 'alfaprevenir', clave: 'alfa2026' }
 ];
