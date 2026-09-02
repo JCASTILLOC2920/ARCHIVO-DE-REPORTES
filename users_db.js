d34b608d5e5f816b754ff84f0b5d59e5f2920b1e
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-09-02 18:27:27 -0500

    Restauracion 100% fiel de las contraseñas originales de la tabla del usuario en users_db.js

--- users_db.js
+++ users_db.js
@@ -1,11 +1,11 @@
 export const usersDatabase = [
-    { id: 1, perfil: 'Administrador', dni: '41457466', nombres: 'JOSEHP CHRISTOPHER CASTILLO CUENCA', usuario: '41457466', clave: 'josehp789', claveHash: 'c86399dfc4d911d4f1c2df9436e44fabe00b0f32ed6de93293bffbd89e506d0c' },
-    { id: 2, perfil: 'Usuario', dni: '', nombres: 'CLINICA LA MUJER', usuario: 'Mujersegura', clave: 'mujer2026', claveHash: '6621d47d045a6bb44c98c0f342606c7ee248652c38837d1b3e553222417bfe8b' },
-    { id: 3, perfil: 'Usuario', dni: '', nombres: 'CLÍNICA CARRIÓN', usuario: 'clinicacarrion', clave: 'carrion2026', claveHash: '6621d47d045a6bb44c98c0f342606c7ee248652c38837d1b3e553222417bfe8b' },
-    { id: 4, perfil: 'Usuario', dni: '', nombres: 'CLÍNICA SAN CLEMENTE', usuario: 'sanclemente', clave: 'sanclemente2026', claveHash: 'abeb7d950dc98c7702a478fd9476563cdf5f6c5fcc99aadd7c08612448aec684' },
-    { id: 5, perfil: 'Usuario', dni: '', nombres: 'SR JUNCO', usuario: 'JUNCO2026', clave: 'junco2026', claveHash: '461c2204b02ce179f9ce8198fe0201c872af08e7f9d6d41bcd7af2d4fbb98e30' },
-    { id: 6, perfil: 'Administrador', dni: '', nombres: 'NICOLAS CASTILLO DIAZ', usuario: 'viringo', clave: 'viringo2026', claveHash: 'abeb7d950dc98c7702a478fd9476563cdf5f6c5fcc99aadd7c08612448aec684' },
-    { id: 7, perfil: 'Usuario', dni: '', nombres: 'DR. DIEGO ALONSO CHUNGUI BRAVO', usuario: 'drdiegochungui', clave: 'chungui2026', claveHash: '81a95f6a72b9d8ccad59bdd65196771b3d0183e1a135be53dbac80abba2d841a' },
+    { id: 1, perfil: 'Administrador', dni: '41457466', nombres: 'JOSEHP CHRISTOPHER CASTILLO CUENCA', usuario: '41457466', clave: 'Betasporina1g', claveHash: 'c86399dfc4d911d4f1c2df9436e44fabe00b0f32ed6de93293bffbd89e506d0c' },
+    { id: 2, perfil: 'Usuario', dni: '', nombres: 'CLINICA LA MUJER', usuario: 'Mujersegura', clave: '212523', claveHash: '6621d47d045a6bb44c98c0f342606c7ee248652c38837d1b3e553222417bfe8b' },
+    { id: 3, perfil: 'Usuario', dni: '', nombres: 'CLÍNICA CARRIÓN', usuario: 'clinicacarrion', clave: '212523', claveHash: '6621d47d045a6bb44c98c0f342606c7ee248652c38837d1b3e553222417bfe8b' },
+    { id: 4, perfil: 'Usuario', dni: '', nombres: 'CLÍNICA SAN CLEMENTE', usuario: 'sanclemente', clave: '112603', claveHash: 'abeb7d950dc98c7702a478fd9476563cdf5f6c5fcc99aadd7c08612448aec684' },
+    { id: 5, perfil: 'Usuario', dni: '', nombres: 'SR JUNCO', usuario: 'JUNCO2026', clave: 'JUNCO2026', claveHash: '461c2204b02ce179f9ce8198fe0201c872af08e7f9d6d41bcd7af2d4fbb98e30' },
+    { id: 6, perfil: 'Administrador', dni: '', nombres: 'NICOLAS CASTILLO DIAZ', usuario: 'viringo', clave: '112603', claveHash: 'abeb7d950dc98c7702a478fd9476563cdf5f6c5fcc99aadd7c08612448aec684' },
+    { id: 7, perfil: 'Usuario', dni: '', nombres: 'DR. DIEGO ALONSO CHUNGUI BRAVO', usuario: 'drdiegochungui', clave: 'diego2026', claveHash: '81a95f6a72b9d8ccad59bdd65196771b3d0183e1a135be53dbac80abba2d841a' },
     { id: 8, perfil: 'Usuario', dni: '', nombres: 'CLÍNICA ALFA PREVENIR', usuario: 'alfaprevenir', clave: 'alfa2026', claveHash: '5aaafb6d4e605f2a1c9e1ef65e7c09de7c523c1989a450a0e6534323eee08c6e' }
 ];
 
