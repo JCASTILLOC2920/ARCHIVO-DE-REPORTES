7af653edb7688da5dc3e693eadec6adf6abb588b
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-09-06 04:34:46 -0500

    s

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { initTableUI, renderTable, applyFilters, setCurrentService } from './ui_
 import { initModalListeners, openModal, closeModal } from './ui_editor.js';
 import { openPrintWindow } from './pdf_engine.js';
 import { initDictaphone, startDictation } from './dictaphone_core.js';
-import { initReportEditorLogic, populateEditorModal } from './ui_report_editor.js?v=561.00';
+import { initReportEditorLogic, populateEditorModal } from './ui_report_editor.js?v=562.00';
 import { initAdminUI, populateModalDoctorsSelect } from './ui_admin.js';
 
 
