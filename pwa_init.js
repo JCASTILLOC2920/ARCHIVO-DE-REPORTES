0db6442b59d8c635f8b61ef1a11b3bb5c434a047
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-09-08 22:11:27 -0500

    S

--- pwa_init.js
+++ pwa_init.js
@@ -5,11 +5,11 @@
     // 1. Registro del Service Worker Médico con Bypass de Caché
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function() {
-            navigator.serviceWorker.register('sw.js?v=576.00', { scope: './', updateViaCache: 'none' })
+            navigator.serviceWorker.register('sw.js?v=577.00', { scope: './', updateViaCache: 'none' })
                 .then(function(registration) {
                     // Forzar comprobación inmediata de actualización
                     registration.update();
-                    console.log('[PWA Medical] Service Worker registrado con éxito (v576). Scope:', registration.scope);
+                    console.log('[PWA Medical] Service Worker registrado con éxito (v577). Scope:', registration.scope);
                     
                     // Escuchar actualizaciones en segundo plano
                     registration.addEventListener('updatefound', function() {
